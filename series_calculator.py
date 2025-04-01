cef32c0ec1282adfc986f08eb4f9ea4e6f862a3e
Wesley Patten <wespattenachill@gmail.com>
2025-04-01 12:51:12 +0200

    Comments added

--- series_calculator.py
+++ series_calculator.py
@@ -1,17 +1,22 @@
+import math
+# function for addition
 def addition(a,b):
     return a + b
 
+# function for subtraction
 def subtraction(a,b):
     return a - b
 
+# function for multiplication
 def multiplication(a,b):
     return a * b
 
+# function for division
 def division(a,b):
     if b == 0:
         return "Error: Division by Zero"
-    return a / b
 
+# calculation function
 def calculation(op,num1,num2):
     if op == "+":
         return addition(num1,num2)
@@ -27,12 +32,14 @@ def calculation(op,num1,num2):
     
     else:
         return "Error: Invalid Operation"
-    
+
+# getting user input    
 num1 = float(input("Enter a number: "))
 num2 = float(input("Enter a number: "))
 op = input("Enter an operation to perform, '+','-','*','/'")
 
 
 if __name__ == "__main__":
+    # assigning result to the called function, printing the result.
     result = calculation(op,num1,num2)
     print(result)
--- simple_calculator.py
+++ simple_calculator.py
@@ -1,4 +1,4 @@
-# This program is a simple calculator that does the four simple operations + - * / and give the answer
+# This program is a simple calculator that does one of the four simple operations + - * / and give the answer
 """ 
 Psuedocode
 1. Create a function to get the input of a number.
@@ -8,6 +8,8 @@ Psuedocode
 5. Perform the calculation and store the answer.
 6. Print out the answer.
 """
+# defining functions for number and operator input
+
 def number_input():
     number = input("Enter a number: ")
     number = int(number)
@@ -17,12 +19,15 @@ def operation_choice():
     operation = input("Choose the operation you would like to perform '+' '-' '*' or '/': ")
     return operation
 
+# defining the calculation function
 def calculation():
+    # creating an answer variable and assigning and calling the number and operation functions to variables
     answer = 0
     num_1 = number_input()
     operator = operation_choice()
     num_2 = number_input()
 
+# Logic for the four operations
     if operator == "+":
         answer = num_1 + num_2
         print(f"The answer to {num_1} {operator} {num_2} = {answer}")
